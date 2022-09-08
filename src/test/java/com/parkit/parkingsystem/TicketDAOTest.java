03ec9a0ba43bbe7c8cf86e2061943057064173a7
DevPC <a.roche.c@gmail.com>
2022-09-08 16:26:03 +0200

    Mise en place de la structure des tests référant à TicketDAO

---
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -0,0 +1,39 @@
+package com.parkit.parkingsystem;
+
+import net.bytebuddy.NamingStrategy;
+import org.junit.jupiter.api.Test;
+
+public class TicketDAOTest {
+
+    @Test
+    void saveTicketTest() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+    }
+
+    @Test
+    void getTicketTest() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+
+    }
+    @Test
+    void updateTicketTest() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+    }
+
+
+}
